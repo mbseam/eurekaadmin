04e35b008a219b0c860866c7ffa7e50e0e590bb5
mbseam <mbseam@gmail.com>
2021-12-25 23:21:08 +0300

    some stuff

---
+++ src/main/java/com/mb/admin/App.java
@@ -0,0 +1,13 @@
+package com.mb.admin;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
